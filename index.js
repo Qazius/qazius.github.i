a3923ef49502e9c0ad8813e48583025af78c1cf8
qaz <qaziusx@gmail.com>
2022-12-05 18:34:15 +0100

    typo

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var NumOfStacks;
-var Rest;
-
 function submit() {
-NumOfStacks = document.getElementByID("stack-of-64").value;
-Rest = documemt.getElementByID("partial-stack").value;
+var NumOfStacks = document.getElementByID("stack-of-64").value;
+var Rest = document.getElementByID("partial-stack").value;
 document.getElementByID("total-items").innerText = +NumOfStacks * 64 + +Rest;
 };
