78cd0f8ea47d2674b85cbe1ff36e4e1face0edee
qaz <qaziusx@gmail.com>
2022-12-05 18:43:30 +0100

    ok?

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function submit() {
-var NumOfStacks = document.getElementByID("stacks-of-64").value;
-var Rest = document.getElementByID("partial-stack").value;
-document.getElementByID("total-items").innerText = +NumOfStacks * 64 + +Rest;
-};
+   var NumOfStacks = document.getElementByID("stacks-of-64").value;
+   var Rest = document.getElementByID("partial-stack").value;
+   document.getElementByID("total-items").innerText = +NumOfStacks * 64 + +Rest;
+}
